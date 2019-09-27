f1ef501761902fb477cdb01e26ef3526d5479093
talynnpm <talynn@scripted.org>
2019-09-27 16:55:45 +0000

    Day 1 of project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $( "document" ).ready(function() {
-
-
-
+    $("button").click(function(){
+    let userInput = $("input").val();
+    let slicer = userInput.slice(1);
+    let pigLatin = userInput.charAt(0);
+    $(".output").text(`${slicer}${pigLatin}ay`);
+    });
 });
